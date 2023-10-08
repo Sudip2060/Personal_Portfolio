24cb57247afbcadee6de20c0cc51757c7234064b
Sudip2060 <sudipghimire9844@gmail.com>
2023-10-08 05:27:51 -0400

    File structuring and renaming
    index.js and other files have been arranged for better understanding. Partial files for header and footer have been created. The main CSS file has been renamed from demo.css to design.css

--- Src/index.js
+++ Src/index.js
@@ -3,15 +3,14 @@ const app = express();
 
 app.set('view engine','ejs')
 
-app.use(express.static('views'))
+app.use(express.static('public'))
 
 app.get('/',(req,res)=> {res.render('home')});
 app.get('/home',(req,res)=> {res.render('home')});
-
+app.get('/Projects',(req,res)=>{res.render('Projects')});
 app.get('/aboutme',(req,res)=>{res.render('aboutme')})
-
 app.get('/contact',(req,res)=>{res.render('Contact')})
 
 const port = 5000;
-app.listen(port,()=>console.log('Applicaiton is running in 5000 port')
+app.listen(port,()=>console.log('Applicaiton is running in 5000 port: http://localhost:5000/')
     ); 

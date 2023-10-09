1e25f5cf2971dcabb188f40b890c38c4f15b0962
Sudip2060 <sudipghimire9844@gmail.com>
2023-10-09 08:46:18 -0400

    Adding comments and Creating the about me page.
    Several Comments were added to make the code friendly and promptly understandable. Changes were made also in project page where the actual project photos , their descriptions and links to open them were added.

--- Src/index.js
+++ Src/index.js
@@ -1,16 +1,24 @@
+//Import the Express Js library and create an Express Application
 const express = require('express');
 const app = express();
 
+//Set the engine view to Embedded Javascript for rendering dynamic views
 app.set('view engine','ejs')
 
+//Serve static files such as Css and Img from the 'Public' Directory
 app.use(express.static('public'))
 
+
+//Defining routes for different pages of the web application
 app.get('/',(req,res)=> {res.render('home')});
 app.get('/home',(req,res)=> {res.render('home')});
 app.get('/Projects',(req,res)=>{res.render('Projects')});
 app.get('/aboutme',(req,res)=>{res.render('aboutme')})
 app.get('/contact',(req,res)=>{res.render('Contact')})
 
+//Assigning Port number as 5000 for the express application to listen on
 const port = 5000;
+
+//start the express application and listen on the assigned port
 app.listen(port,()=>console.log('Applicaiton is running in 5000 port: http://localhost:5000/')
     ); 
